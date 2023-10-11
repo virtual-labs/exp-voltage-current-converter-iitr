3c8c7c01dc2dad0a8f292c069e9373ba8153546c
rajeevfee <68722924+rajeevfee@users.noreply.github.com>
2023-10-11 17:12:22 +0530

    Update

--- experiment/simulation/js/demo.js
+++ experiment/simulation/js/demo.js
@@ -6,6 +6,13 @@
     yy.onclick=checkk;
     function checkk()
     {
+
+       if(connections.length ==0) {
+        alert ("Please make the connections first");
+        return false;
+       }
+
+
        if(connections.length<4)
        {alert("Wrong Connections\nPlease go through the instructions once")
        return false}
